6ab3787aae486ce1a49a7dabdd9b6b8df9912b15
hieund3 <hieund3@vng.com.vn>
2021-10-15 14:26:18 +0700

    add heap lecture

--- 7_heap/the_lazy_programmer.py
+++ 7_heap/the_lazy_programmer.py
@@ -3,15 +3,59 @@ Link:
 Time complexity: O(n)
 Space complexity: O(n)
 """
+import heapq
+
+
+class Job:
+    def __init__(self, a, b, d):
+        self.d = d
+        self.b = b
+        self.a = a
+        self.total_run = 0
+
+    def __lt__(self, other):
+        return self.a > other.a
+
+    def set_total_run(self, total_run):
+        self.total_run = total_run
+
 
 if __name__ == '__main__':
     num_testcase = int(input())
     for _ in range(num_testcase):
         N = int(input())
+        job_list = []
+        heap = []
         total = 0
         for i in range(N):
             a, b, d = map(int, input().split())
-            extend = (b - d) / a
-            if extend > 0:
-                total += extend
+            job_list.append(Job(a, b, d))
+        job_list = sorted(job_list, key=lambda x: x.d)
+        ts = 0
+        for i in range(len(job_list)):
+            if ts + job_list[i].b <= job_list[i].d:
+                job_list[i].set_total_run(job_list[i].b)
+                heapq.heappush(heap, job_list[i])
+            else:
+                need_time = ts + job_list[i].b - job_list[i].d
+                while need_time != 0:
+                    if len(heap) > 0 and heap[0].a > job_list[i].a:
+                        val = heapq.heappop(heap)
+                        if val.total_run > need_time:
+                            need_money = need_time / val.a
+                            val.total_run = val.total_run - need_time
+                            total += need_money
+                            need_time = 0
+                            heapq.heappush(heap, val)
+                        else:
+                            need_money = val.total_run / val.a
+                            need_time = need_time - val.total_run
+                            total += need_money
+                    else:
+                        need_money = need_time / job_list[i].a
+                        total += need_money
+                        job_list[i].set_total_run(job_list[i].d - ts)
+                        heapq.heappush(heap, job_list[i])
+                        need_time = 0
+            ts = job_list[i].d
         print("%.2f" % total)
