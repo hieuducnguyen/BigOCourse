27bc1c5f93650891c072cbf185e6bf8918b4a4c7
hieund3 <hieund3@vng.com.vn>
2021-10-16 17:41:33 +0700

    review roy_and_trending_topics.py

--- 7_heap/roy_and_trending_topics.py
+++ 7_heap/roy_and_trending_topics.py
@@ -1,7 +1,7 @@
 """
-Link:
-Time complexity: O(n)
-Space complexity: O(n)
+Link: https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/roy-and-trending-topics-1/
+Time complexity: O(N * log(5) + 5 * log(5))
+Space complexity: O(N)
 """
 import heapq
 
@@ -14,8 +14,8 @@ class EntryTopic:
 
     def __lt__(self, other):
         if self.change_score == other.change_score:
-            return self.id > other.id
-        return self.change_score > other.change_score
+            return self.id < other.id
+        return self.change_score < other.change_score
 
 
 if __name__ == '__main__':
@@ -25,8 +25,11 @@ if __name__ == '__main__':
         id, old_zcore, mention, like, comment, share = map(int, input().split())
         new_score = mention * 50 + like * 5 + comment * 10 + share * 20
         entry = EntryTopic(id, new_score - old_zcore, new_score)
-        heap.append(entry)
-    heapq.heapify(heap)
-    for _ in range(5):
-        entry = heapq.heappop(heap)
-        print(entry.id, entry.new_score)
+        if len(heap) < 5:
+            heapq.heappush(heap, entry)
+        else:
+            heapq.heappush(heap, entry)
+            heapq.heappop(heap)
+    heap = sorted(heap, key=lambda x: (-x.change_score, -x.id))
+    for _ in range(len(heap)):
+        print(heap[_].id, heap[_].new_score)
