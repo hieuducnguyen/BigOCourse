55fd95e40aa07c7ccd307335ea52ef95f6c43033
hieund3 <hieund3@vng.com.vn>
2021-10-16 22:27:06 +0700

    review restaurant_rating.py

--- 7_heap/restaurant_rating.py
+++ 7_heap/restaurant_rating.py
@@ -1,11 +1,15 @@
 """
-Link:
-Time complexity: O(n)
-Space complexity: O(n)
+Link: https://www.codechef.com/problems/RRATING
 """
 import heapq
 
-if __name__ == '__main__':
+"""
+Time complexity: O(N * log(N))
+Space complexity: O(N)
+"""
+
+
+def method_1():
     N = int(input())
     min_heap_top = []
     max_heap_remain = []
@@ -17,7 +21,6 @@ if __name__ == '__main__':
                 print("No reviews yet")
             else:
                 print(-max_heap_remain[0])
-
         else:
             operation, value = map(int, input_text.split())
             size_post += 1
@@ -34,3 +37,42 @@ if __name__ == '__main__':
                     heapq.heappush(min_heap_top, - pop_value)
             else:
                 heapq.heappush(max_heap_remain, -value)
+
+
+"""
+Time complexity: O(N * log(N))
+Space complexity: O(N)
+"""
+
+
+def method_2():
+    N = int(input())
+    min_heap = []
+    max_heap = []
+    size_post = 0
+    for _ in range(N):
+        query = input()
+        if query == "2":
+            if size_post > 2:
+                print(min_heap[0])
+            else:
+                print("No reviews yet")
+        else:
+            op, val_string = query.split()
+            val = int(val_string)
+            size_post += 1
+            if size_post == 1:
+                heapq.heappush(max_heap, -val)
+                continue
+            if - max_heap[0] >= val:
+                heapq.heappush(max_heap, -val)
+            else:
+                heapq.heappush(min_heap, val)
+            if len(min_heap) < size_post // 3:
+                heapq.heappush(min_heap, -heapq.heappop(max_heap))
+            elif len(min_heap) > size_post // 3:
+                heapq.heappush(max_heap, -heapq.heappop(min_heap))
+
+
+if __name__ == '__main__':
+    method_2()
